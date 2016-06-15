e32018911c31124d5f62b497c4dd8b515d755708
Rey Vera <veruxdev@gmail.com>
2016-06-15 09:28:20 -0700

    Add google-maps-react to the project

--- client/map/MapWrapper.jsx
+++ client/map/MapWrapper.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
-export default class ContentWrapper extends TrackerReact(Component) {
+import Map from 'google-maps-react';
+
+export default class MapWrapper extends TrackerReact(Component) {
 	render() {
 		return (
 			<div>
 				<h1>Map goes here!</h1>
+				<Map google={this.props.google} zoom={14}>
+				</Map>
 			</div>
 		)
 	}
