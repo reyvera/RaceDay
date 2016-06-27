cddaae9b09bc11e792afe9ddc4289cf79c915c3c
Rey Vera <veruxdev@gmail.com>
2016-06-27 12:19:48 -0700

    Init iOS

--- raceDay_ReactNative/index.ios.js
+++ raceDay_ReactNative/index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -17,14 +11,7 @@ class raceDay_ReactNative extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Welcome to raceDay!
         </Text>
       </View>
     );
