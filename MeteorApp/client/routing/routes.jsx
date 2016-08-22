919c29877b1ea5f5fc8c0222574b967433142d1b
Rey Vera <veruxdev@gmail.com>
2016-08-22 10:44:14 -0700

    Meteor: Update Meteor packages

--- MeteorApp/client/routing/routes.jsx
+++ MeteorApp/client/routing/routes.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import {mount} from 'react-mounter';
 
+import { Accounts } from 'meteor/accounts-base';
+
 import {MainLayout} from '../layout/MainLayout';
 
 import MapWrapper from '../map/MapWrapper';
 import Home from '../home/Home';
+
+/* Set to Username only */
+// Accounts.ui.config({
+//   passwordSignupFields: 'USERNAME_ONLY',
+// });
 
 FlowRouter.route('/', {
 	action() {
