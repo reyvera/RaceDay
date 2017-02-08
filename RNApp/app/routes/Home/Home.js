c1736ad68cf9ca1723f74ec4684f2987261c7b6a
Rey Vera <veruxdev@gmail.com>
2017-02-08 14:56:43 -0800

    RNApp: Add Mapbox
    Add Mapbox support to Home screen

--- RNApp/app/routes/Home/Home.js
+++ RNApp/app/routes/Home/Home.js
@@ -3,16 +3,23 @@ import { Text, View } from 'react-native';
 import Button from '../../components/Button';
 import styles from './styles';
 
+import Mapbox, { MapView } from 'react-native-mapbox-gl';
+
+const accessToken = 'pk.eyJ1IjoidmVydXhkZXYiLCJhIjoiY2l1cHU3aGpwMDF0cTJvcGVpYnMycTNqdCJ9.LVfTjQc_XiWY-bXn9s20gQ';
+Mapbox.setAccessToken(accessToken);
+
 const Home = (props) => {
+	onUpdateUserLocation = (location) => {
+		console.log('onUpdateUserLocation', location);
+	};
+
 	return (
 		<View style={styles.container}>
-			<Text style={styles.main}>
-				Home
-			</Text>
-			<Button
-				text="Details"
-				onPress={props.onDetailsPress}
-			/>
+			<MapView 	ref={map => { this._map = map; }}
+								style={styles.map}
+								showsUserLocation={true}
+								userTrackingMode={Mapbox.userTrackingMode.follow}
+								onUpdateUserLocation={this.onUpdateUserLocation} />
 		</View>
 	);
 };
